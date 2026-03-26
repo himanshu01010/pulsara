ea4e76978e59cacc9326ec8ecc2b04e331d53edb
himanshu01010 <himanshubehera08052003@gmail.com>
2026-03-26 13:11:57 +0530

    python config

--- Trendzzpy/app/config/settings.py
+++ Trendzzpy/app/config/settings.py
@@ -2,11 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    GEMINI_API_KEY: str
     MAX_TRENDS_TO_PROCESS: int = 10
-    # UNSPLASH_ACCESS_KEY: str | None = None
-    # GEMINI_MODEL: str = "gemini-2.5-flash"
-    # GEMINI_MAX_TOKENS: int = 1200
     MODEL:str="llama-3.1-8b-instant"
     HF_TOKEN: str
     HF_API_URL:str
